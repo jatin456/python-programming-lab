7ff018da1541fd537e43257ccc07e071acbca90b
jatin456 <92090647+jatin456@users.noreply.github.com>
2022-05-24 19:24:41 +0530

    Add files via upload

---
+++ (list)shopping list.py
@@ -0,0 +1,14 @@
+list1=[]
+while(1):
+    a=input("enter item: ")
+    if a=='':
+        break
+    
+    list1.append(a)
+print(list1)
+print()
+print("Here is your list")
+for i in list1:
+    print(i)
+
+   
